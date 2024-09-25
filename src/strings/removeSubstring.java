fb325708f2d9eb8d4d4e7639c6925410753afaad
rxman <k.romann413@gmail.com>
2024-09-25 04:06:36 -0700

    permutation and remove all occurence: strings

---
+++ src/strings/removeSubstring.java
@@ -0,0 +1,46 @@
+package strings;
+
+import java.util.Stack;
+
+public class removeSubstring {
+
+    public static void main(String[] args) {
+        String str = "aabababa";
+        String subStr = "aba";
+
+        String newStr = removeFromUsingStack (str, subStr);
+        System.out.println("New String: " + newStr);
+
+    }
+
+
+
+    private static String removeFromUsingStack (String s, String part) {
+
+        StringBuilder sb = new StringBuilder();
+        int m;
+        int n = part.length();
+
+        for (char ch : s.toCharArray()) {
+            sb.append(ch);
+            m = sb.length();
+            if ((m >= n) && sb.substring(m - n).equals(part) ) {
+                sb.delete(m-n, m);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private static String removeFrom(String s, String part) {
+
+        int index = s.indexOf(part);
+        while (index != -1) {
+            System.out.println(s.substring(0, index) +" "+ s.substring(index + part.length()));
+            s = s.substring(0, index) + s.substring(index + part.length());
+            index = s.indexOf(part);
+        }
+        return s;
+    }
+
+}
