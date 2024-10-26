e97cfce7498f734c491004085c613c8b81d64443
rxman <k.romann413@gmail.com>
2024-10-26 12:54:00 +0545

    Largest rectangular area in a histogram

--- src/stacks/hard/largestAreaOfHistogram.java
+++ src/stacks/hard/largestAreaOfHistogram.java
@@ -3,6 +3,7 @@ package stacks.hard;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 import static stacks.hard.smallerNextElement.nextSmallerElement;
 import static stacks.hard.smallerNextElement.previousSmallerElement;
@@ -10,30 +11,67 @@ import static stacks.hard.smallerNextElement.previousSmallerElement;
 public class largestAreaOfHistogram {
     public static void main(String[] args) {
 
-        List<Integer> arr = Arrays.asList(2,1,5,6,2,3);
-        int result = findLargestAreaInHistogram(arr);
+        int[] arr = new int[]{2,1,5,6,2,3, 2, 3, 2};
+        System.out.println(Arrays.toString(arr));
+        int result = largestRectangleArea(arr);
         System.out.println(result);
 
     }
 
-    static int findLargestAreaInHistogram (List<Integer> arr) {
+    static int[] prevSmaller (int[] heights) {
 
-        int ans = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int[] result = new int[heights.length];
 
-        ArrayList<Integer> nextSmallest = nextSmallerElement(arr, arr.size());
-        ArrayList<Integer> previousSmallest = previousSmallerElement(arr, arr.size());
 
-        for (int i=0; i<arr.size(); i++) {
-            int length = arr.get(i);
-
-            if (nextSmallest.get(i) == -1) {
-                nextSmallest.set(i, arr.size());
+        for (int i=0; i<heights.length; i++) {
+            int current  = heights[i];
+            while (!stack.isEmpty() && stack.peek() != -1 && heights[stack.peek()] >= current) {
+                stack.pop();
             }
-            int breadth = nextSmallest.get(i) - previousSmallest.get(i) + 1;
 
-            int newArea = length * breadth;
+            result[i] = stack.peek();
+            stack.push(i);
+        }
 
-            ans = Math.max(newArea, ans);
+        return result;
+    }
+
+
+    static int[] nextSmaller (int[] heights) {
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int[] result = new int[heights.length];
+
+
+        for (int i=heights.length-1; i>=0; i--) {
+            int current  = heights[i];
+            while (!stack.isEmpty() && stack.peek() != -1 && heights[stack.peek()] >= current) {
+                stack.pop();
+            }
+
+            result[i] = stack.peek();
+            stack.push(i);
+        }
+
+        return result;
+    }
+
+    public static int largestRectangleArea(int[] heights) {
+        int ans = -1;
+
+        int[] prev = prevSmaller(heights);
+        int[] next = nextSmaller(heights);
+
+        for (int i=0; i<heights.length; i++) {
+            if (next[i] == -1 ) {
+                next[i] = heights.length;
+            }
+            int width = next[i] - prev[i] - 1;
+            int tempArea = width * heights[i];
+            ans = Math.max(tempArea, ans);
         }
 
         return ans;
