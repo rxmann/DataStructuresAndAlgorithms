7e5a4b640c6213668630d23d2322846130ce2c8d
rxmann <k.romann413@gmail.com>
2025-03-01 13:26:51 +0545

    feat(): dates in java

---
+++ src/dates/DatesAndTimes.java
@@ -0,0 +1,36 @@
+package dates;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class DatesAndTimes {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+        Instant instant = Instant.now();
+        Date date = new Date();
+        LocalDate ld = LocalDate.now();
+        LocalTime lt = LocalTime.now();
+        LocalDateTime ldt = LocalDateTime.now();
+        ZonedDateTime zdt = ZonedDateTime.now();
+
+        System.out.println("Date: " + date);
+        System.out.println("LocalDate: " + ld);
+        System.out.println("LocalTime: " + lt);
+        System.out.println("LocalDateTime: " + ldt);
+        System.out.println("ZonedDateTime: " + zdt);
+        System.out.println("Instant: " + instant);
+        System.out.println("Date: " + date);
+        // Get year, month, and day from the date object
+        int YEAR = ldt.getYear();
+        int MONTH = ldt.getMonthValue();
+        int DAY = ldt.getDayOfMonth();
+        System.out.println("LocalDateTime YEAR: " + YEAR);
+        System.out.println("LocalDateTime MONTH: " + MONTH);
+        System.out.println("LocalDateTime DAY: " + DAY);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String formattedDate = ld.format(formatter);
+        System.out.println(formattedDate);
+    }
+}
+
